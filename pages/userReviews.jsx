f5f02a1bbf7a12926112a24d3abc59bee4d23fa5
Luis Angeles <a01273884@itesm.mx>
2020-06-01 18:06:24 -0500

    userReviews
    Show the users Reviews

--- pages/userReviews.jsx
+++ pages/userReviews.jsx
@@ -8,7 +8,6 @@ import {
 	FormControlLabel,
 	FormGroup,
 	Checkbox,
-	Button,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import useSWR from "swr";
@@ -17,10 +16,26 @@ import * as yup from "yup";
 import { useSnackbar } from "notistack";
 import Layout from "../components/layout/Layout";
 import { useUser } from "../lib/hooks";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(() => ({}));
+
+const useStyles = makeStyles(() => ({
+    root: {
+        maxWidth: 345,
+      },
+      media: {
+        height: 140,
+      },
+}));
+
 
 const Page = () =>{
+    const classes = useStyles();
     const [user] = useUser();
 	const { enqueueSnackbar } = useSnackbar();
 	
@@ -41,11 +56,46 @@ const Page = () =>{
     const reviews = data?.reviews || {};
    
     console.log(reviews);
-    return(
+    
+    if(reviews){
+    return (
         <Layout>
-
+            <h1 style={{textAlign:"center"}}>Mis Reseñas</h1>
+            {reviews.map((review) =>{
+                return(
+                <Card className={classes.root} style={{margin:"10px"}}>
+                    <CardActionArea>
+                        <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="p" component="h2">
+                            Reseña
+                        </Typography>
+                        <Typography gutterBottom variant="p" component="h3">
+                            Lugar: {review.place}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {review.content}
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                        Share (NA for the moment)
+                        </Button>
+                       
+                    </CardActions>
+            </Card>);
+            })}
+            
         </Layout>
-    );
+      );}
+      else{
+        return <Typography>Loading</Typography>;
+      }
 }
 
 
