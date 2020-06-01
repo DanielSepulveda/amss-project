135832e24aec43094b3a9f13a68295284598f554
Luis Angeles <a01273884@itesm.mx>
2020-05-31 19:16:40 -0500

    Preferencias
    Changes in preferencias

--- pages/preferencias.jsx
+++ pages/preferencias.jsx
@@ -7,7 +7,12 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import Layout from 'components/layout/Layout';
 import {Button} from '@material-ui/core'
+import { useUser } from "../lib/hooks";
+import Router from "next/router";
+import { useSnackbar } from "notistack";
+import useSWR from 'swr';
 
+var flag = false;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,55 +22,103 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function CheckboxList() {
+const userCheck = () => {
+	const [user] = useUser();
+	React.useEffect(() => {
+		if (user===null || flag ==true) {
+			Router.replace("/login");
+		}
+	}, [user]);
+}
+
+//obtener categorias existentes
+CheckboxList.getInitialProps = async() =>{
+  const res = await fetch('http://localhost:3000/api/category');
+  const {data} = await res.json();
+  
+  return {category: data};
+}
+
+export default function CheckboxList({category}) {
+    userCheck();
+    const [user, {mutate}] = useUser();
+    const { enqueueSnackbar } = useSnackbar();
+
+    console.log(user);
+    
   const classes = useStyles();
   const [checked, setChecked] = React.useState([0]);
 
   const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
+    const currentIndex = checked.indexOf(value._id);
     const newChecked = [...checked];
     if (currentIndex === -1) {
-      newChecked.push(value);
+      newChecked.push(value._id);
     } else {
       newChecked.splice(currentIndex, 1);
     }
-
     setChecked(newChecked);
   };
+
+  //mandar la forma
+  const handleSubmit = async() =>{
+      var info = [...checked];
+      var infoSend ={"likes": info};
+      infoSend = JSON.stringify(infoSend);
+      console.log(infoSend);
+    try {
+			const res = await fetch("/api/users/"+user._id, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json"
+				},
+                body: infoSend,
+            });
+            if (!res.ok) throw new Error(await res.text());
+            enqueueSnackbar("Preferences Registered", { variant: "success" });
+            flag = true;
+		} catch (e) {
+            enqueueSnackbar(e.message, { variant: "error" });
+		}
+  }
+
+
 
   return (
     <Layout>
         <h1 style={{textAlign:"center", fontSize:"2.5em", marginTop:"0"}}>Elige tus Preferencias</h1>
-        
-        <List style={{width:"100%", overflow:"auto"}}>
-            {[0,1,2,3,4,5,6,7,8,9,10].map((value) => { //cantidad de elementos de la lista
-                const labelId = `checkbox-list-label-${value}`;
+            <List style={{width:"100%", overflow:"auto"}}>
+                {category.map((cat) => { 
+                    const labelId = `checkbox-list-label-${cat}`;
 
-                return (
-                <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)} 
-                style={{width:"100%", textAlign:"center", overflow:"auto"}}>
-                    <ListItemIcon>
-                    <Checkbox
-                        style={{marginLeft:"40%"}}
-                        edge="start"
-                        checked={checked.indexOf(value) !== -1}
-                        tabIndex={-1}
-                        disableRipple
-                        inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                    </ListItemIcon>
-                    <ListItemText id={labelId} primary={`Tipo de Comida ${value + 1}`} />
-                </ListItem>
-                );
-            })}
+                    return (
+                    <ListItem key={cat._id} role={undefined} dense button onClick={handleToggle(cat)} 
+                    style={{width:"100%", textAlign:"center", overflow:"auto"}}>
+                        <ListItemIcon>
+                        <Checkbox
+                            style={{marginLeft:"20%"}}
+                            edge="start"
+                            checked={checked.indexOf(cat._id) !== -1}
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                        </ListItemIcon>
+                        <ListItemText id={labelId} primary={`Comida ${cat.nameCategory}`} />
+                    </ListItem>
+                    );
+                })}
+                
+            </List>
             <div style={{textAlign:"center", marginTop:"1em"}}>
-                <Button style={{fontSize:"2em"}} variant="contained" color="secondary" >
-                    Submit Preferences
-                </Button>
+                    <Button style={{fontSize:"2em", marginTop:"3em"}} variant="contained" color="secondary" 
+                      onClick={handleSubmit}
+                      >
+                        Submit Preferences
+                    </Button>
             </div>
-            
-        </List>
-    
+          
+       
     </Layout>
   );
 }
